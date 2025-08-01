fba658c5283c0a199885c4b2cb7c7860156fd2b7
ameer3rabe00 <222779607+ameer3rabe00@users.noreply.github.com>
2025-08-01 19:15:07 +0300

    Add files via upload
    הוספתי התחברות עם הרשמה, ובדקתי עם הקטגוריות וזה עובד טוב עכשיו מאז שהוספתי התחברות.
    
    m5 ‏add slashes salt בשביל הגנה נוספתה שתמשתי ב.
    
     jwt (islogged) עם אימות TaskAppCookie השתמשתי בעוגיות  ***i added this before but in root by mistake  so adding here and deleated in root ***

---
+++ Routers/auth_R.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+const user_Mid = require("../middleware/user_Mid");
+
+router.get("/login", (req, res) => {
+    res.render("login", {});
+});
+
+router.post("/login", [user_Mid.CheckLogin], (req, res) => {
+    if (req.validUser) {
+        res.redirect("/tasks/list");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+router.get("/register", (req, res) => {
+    res.render("register", {});
+});
+
+router.post("/register", [user_Mid.AddUser], (req, res) => {
+    if (req.userAdded) {
+        res.redirect("/login");
+    } else {
+        res.redirect("/register");
+    }
+});
