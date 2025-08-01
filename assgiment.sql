3a3be9f1a03b1a840c4208a910187022bec0b0f8
ameer3rabe00 <222779607+ameer3rabe00@users.noreply.github.com>
2025-08-01 18:09:32 +0300

    Add files via upload
    הוספתי קצת עיצוב בקטגוריות, התחברות והרשמה, והוספתי את הנתונים עבור המשימות. בהמשך אתחיל לבנות את הוספת המשימות וסינון

--- assgiment.sql
+++ assgiment.sql
@@ -13,3 +13,16 @@ CREATE TABLE categories (
     name VARCHAR(100),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    description VARCHAR(200),
+    target_date DATE,
+    category_id INT,
+    is_completed TINYINT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
